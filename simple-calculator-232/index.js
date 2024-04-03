74454c9d5a53c97be04d382aec1b728988f9a198
Amna-Javed2 <160821158+Amna-Javed2@users.noreply.github.com>
2024-04-03 08:48:29 -0700

    Add files via upload

---
+++ simple-calculator-232/index.js
@@ -0,0 +1,40 @@
+#! /usr/bin/env node  
+// #! /usr/bin/env node. It is called SHABANG.
+import inquirer from "inquirer"; // inquirer = used to ask questions
+const answer = await inquirer.prompt([
+    {
+        message: "Please enter firstNumber",
+        type: "number",
+        name: "firstNumber",
+    },
+    {
+        message: "Please enter secondNumber",
+        type: "number",
+        name: "secondNumber",
+    },
+    {
+        message: "select one of the operator to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division", "Modulation"],
+    },
+]);
+// conditional statement
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else if (answer.operator === "Modulation") {
+    console.log(answer.firstNumber % answer.secondNumber);
+}
+else {
+    console.log("Please select valid Operator");
+}
